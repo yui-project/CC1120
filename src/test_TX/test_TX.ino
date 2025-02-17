f82ad3d53bd809e8cab64635d920021076e7ede6
kouketsut <kt.class.jar@gmail.com>
2025-02-17 22:01:02 +0900

    Revert "meisou"
    This reverts commit edf60e304d17fd11f7cbea89c5ef93d87cf9d37f.

--- src/test_TX/test_TX.ino
+++ src/test_TX/test_TX.ino
@@ -79,66 +79,37 @@ void loop() {
   Serial.print("PA_CFG2: ");
   Serial.println(readSPI(PA_CFG2), HEX);
  
-  
+  char buffer[128];
   int index = 0;
-  index = 4;
-  // uint8_t buffer[128];
-  uint8_t buffer[4];
-  buffer[0] = 0x00;
-  buffer[1] = 0x56;
-  buffer[2] = 0xAF;
-  buffer[3] = 0xBC;
-  // while (Serial.available() == 0){
-  //   delay(10);
-  // }
-  // while (Serial.available()>0) {
-  //   buffer[index] = Serial.read();
-  //   index++;
-  // }
-
-  delay(1000);
+  while (!Serial.available()){
+    delay(10);
+  }
+  while (Serial.available()) {
+    buffer[index] = Serial.read();
+    index++;
+    //バッファ以上の場合は中断
+    if (index >= 128) {
+      break;
+    }
+  }
 
   for(int i=0; i<4; i++){
-    // for(uint32_t i=0; i<index; i++){
-    //   writeSPI(TXRX_FIFO, buffer[i]);
-    // }
-    // FIFOFlush();
-    readMARCSTATE();
-    // writeSPI(TXRX_FIFO, buffer[0]);
-    // delay(10);
-    // writeSPI(TXRX_FIFO, buffer[0]);
-    // delay(10);
-    // writeSPI(TXRX_FIFO, buffer[0]);
-    // delay(10);
-    // writeSPI(TXRX_FIFO, buffer[0]);
-    // delay(10);
+    for(uint32_t i=0; i<index; i++){
+      writeSPI(TXRX_FIFO, buffer[i]);
+      
+
+    }
     Serial.print("MARCSTATE before STX:  ");
     readMARCSTATE();
-    Serial.println(readExtAddrSPI(NUM_TXBYTES));
-    // strobeSPI(SWOR);  // Enable TX
-    delay(10);
-    strobeSPI(STX);
-    delay(10);
-    writeSPI(TXRX_FIFO, buffer[0]);
-  
-    // Serial.println(readExtAddrSPI(MARC_STATUS0), BIN);
-    // Serial.println(readExtAddrSPI(MARC_STATUS1), BIN);
-    // Serial.println(readExtAddrSPI(MODEM_STATUS0), BIN);
-    // Serial.println(readExtAddrSPI(MODEM_STATUS1), BIN);
-    // Serial.println(readExtAddrSPI(SERIAL_STATUS), BIN);
-    // Serial.print("MARCSTATE before delay:  ");
-    // readMARCSTATE();
+   
+    strobeSPI(STX);  // Enable TX
     delay(3000);
-    
+   
     Serial.print("MARCSTATE after  STX:  ");
     readMARCSTATE();
-    delay(10);
-
-    FIFOFlush();   
-    delay(10);
    
     strobeSPI(SIDLE); // Exit TX/RX, turn off frequency synthesizer and exit eWOR mode if applicable
-    delay(10);
+    FIFOFlush();
  
     delay(10);
     Serial.print("MARCSTATE after SIDLE: ");
@@ -287,181 +258,54 @@ void writeExtAddrSPI(uint8_t addr, uint8_t value) {
 }*/
 
 void configureCC1120_2nd(){
-writeSPI(IOCFG3, IOCFG3_VALUE);
-writeSPI(IOCFG2, IOCFG2_VALUE);
-writeSPI(IOCFG1, IOCFG1_VALUE);
-writeSPI(IOCFG0, IOCFG0_VALUE);
-writeSPI(SYNC3, SYNC3_VALUE);
-writeSPI(SYNC2, SYNC2_VALUE);
-writeSPI(SYNC1, SYNC1_VALUE);
-writeSPI(SYNC0, SYNC0_VALUE);
-writeSPI(SYNC_CFG1, SYNC_CFG1_VALUE);
-writeSPI(SYNC_CFG0, SYNC_CFG0_VALUE);
-writeSPI(DEVIATION_M, DEVIATION_M_VALUE);
-writeSPI(MODCFG_DEV_E, MODCFG_DEV_E_VALUE);
-writeSPI(DCFILT_CFG, DCFILT_CFG_VALUE);
-writeSPI(PREAMBLE_CFG1, PREAMBLE_CFG1_VALUE);
-writeSPI(PREAMBLE_CFG0, PREAMBLE_CFG0_VALUE);
-writeSPI(FREQ_IF_CFG, FREQ_IF_CFG_VALUE);
-writeSPI(IQIC, IQIC_VALUE);
-writeSPI(CHAN_BW, CHAN_BW_VALUE);
-writeSPI(MDMCFG1, MDMCFG1_VALUE);
-writeSPI(MDMCFG0, MDMCFG0_VALUE);
-writeSPI(SYMBOL_RATE2, SYMBOL_RATE2_VALUE);
-writeSPI(SYMBOL_RATE1, SYMBOL_RATE1_VALUE);
-writeSPI(SYMBOL_RATE0, SYMBOL_RATE0_VALUE);
-writeSPI(AGC_REF, AGC_REF_VALUE);
-writeSPI(AGC_CS_THR, AGC_CS_THR_VALUE);
-writeSPI(AGC_GAIN_ADJUST, AGC_GAIN_ADJUST_VALUE);
-writeSPI(AGC_CFG3, AGC_CFG3_VALUE);
-writeSPI(AGC_CFG2, AGC_CFG2_VALUE);
-writeSPI(AGC_CFG1, AGC_CFG1_VALUE);
-writeSPI(AGC_CFG0, AGC_CFG0_VALUE);
-writeSPI(FIFO_CFG, FIFO_CFG_VALUE);
-writeSPI(DEV_ADDR, DEV_ADDR_VALUE);
-writeSPI(SETTLING_CFG, SETTLING_CFG_VALUE);
-writeSPI(FS_CFG, FS_CFG_VALUE);
-writeSPI(WOR_CFG1, WOR_CFG1_VALUE);
-writeSPI(WOR_CFG0, WOR_CFG0_VALUE);
-writeSPI(WOR_EVENT0_MSB, WOR_EVENT0_MSB_VALUE);
-writeSPI(WOR_EVENT0_LSB, WOR_EVENT0_LSB_VALUE);
-writeSPI(PKT_CFG2, PKT_CFG2_VALUE);
-writeSPI(PKT_CFG1, PKT_CFG1_VALUE);
-writeSPI(PKT_CFG0, PKT_CFG0_VALUE);
-writeSPI(RFEND_CFG1, RFEND_CFG1_VALUE);
-writeSPI(RFEND_CFG0, RFEND_CFG0_VALUE);
-writeSPI(PA_CFG2, PA_CFG2_VALUE);
-writeSPI(PA_CFG1, PA_CFG1_VALUE);
-writeSPI(PA_CFG0, PA_CFG0_VALUE);
-writeSPI(PKT_LEN, PKT_LEN_VALUE);
+writeSPI(IOCFG3, IOCFG3_VALUE);        // GPIO3 IO Pin Configuration
+writeSPI(IOCFG2, IOCFG2_VALUE);        // GPIO2 IO Pin Configuration
+writeSPI(IOCFG1, IOCFG1_VALUE);        // GPIO1 IO Pin Configuration
+writeSPI(IOCFG0, IOCFG0_VALUE);        // GPIO0 IO Pin Configuration
+writeSPI(SYNC_CFG1, SYNC_CFG1_VALUE);     // Sync Word Detection Configuration Reg. 1
+writeSPI(DEVIATION_M, DEVIATION_M_VALUE);   // Frequency Deviation Configuration
+writeSPI(MODCFG_DEV_E, MODCFG_DEV_E_VALUE);   // Frequency Deviation Configuration
+writeSPI(DCFILT_CFG, DCFILT_CFG_VALUE);    // Digital DC Removal Configuration
+writeSPI(PREAMBLE_CFG1, PREAMBLE_CFG1_VALUE); // Preamble Length Configuration Reg. 1
+writeSPI(IQIC, IQIC_VALUE);          // Digital Image Channel Compensation Configuration
+writeSPI(CHAN_BW, CHAN_BW_VALUE);       // Channel Filter Configuration
+writeSPI(MDMCFG0, MDMCFG0_VALUE);       // General Modem Parameter Configuration Reg. 0
+writeSPI(SYMBOL_RATE2, SYMBOL_RATE2_VALUE);  // Symbol Rate Configuration Exponent and Mantissa [1..
+writeSPI(SYMBOL_RATE1, SYMBOL_RATE1_VALUE);  // Symbol Rate Configuration Mantissa [15:8]
+writeSPI(SYMBOL_RATE0, SYMBOL_RATE0_VALUE);  // Symbol Rate Configuration Mantissa [7:0]
+writeSPI(AGC_REF, AGC_REF_VALUE);       // AGC Reference Level Configuration
+writeSPI(AGC_CS_THR, AGC_CS_THR_VALUE);    // Carrier Sense Threshold Configuration
+writeSPI(AGC_CFG1, AGC_CFG1_VALUE);      // Automatic Gain Control Configuration Reg. 1
+writeSPI(AGC_CFG0, AGC_CFG0_VALUE);      // Automatic Gain Control Configuration Reg. 0
+writeSPI(FIFO_CFG, FIFO_CFG_VALUE);      // FIFO Configuration
+writeSPI(FS_CFG, FS_CFG_VALUE);        // Frequency Synthesizer Configuration
+writeSPI(PKT_CFG0, PKT_CFG0_VALUE);      // Packet Configuration Reg. 0
+writeSPI(PA_CFG2, PA_CFG2_VALUE);       // Power Amplifier Configuration Reg. 2
+writeSPI(PA_CFG0, PA_CFG0_VALUE);       // Power Amplifier Configuration Reg. 0
+writeSPI(PKT_LEN, PKT_LEN_VALUE);       // Packet Length Configuration
+writeSPI(IF_MIX_CFG, IF_MIX_CFG_VALUE);    // IF Mix Configuration
 
-writeExtAddrSPI(IF_MIX_CFG, IF_MIX_CFG_VALUE);
-writeExtAddrSPI(FREQOFF_CFG, FREQOFF_CFG_VALUE);
-writeExtAddrSPI(TOC_CFG, TOC_CFG_VALUE);
-writeExtAddrSPI(MARC_SPARE, MARC_SPARE_VALUE);
-writeExtAddrSPI(ECG_CFG, ECG_CFG_VALUE);
-writeExtAddrSPI(CFM_DATA_CFG, CFM_DATA_CFG_VALUE);
-writeExtAddrSPI(EXT_CTRL, EXT_CTRL_VALUE);
-writeExtAddrSPI(RCCAL_FINE, RCCAL_FINE_VALUE);
-writeExtAddrSPI(RCCAL_COARSE, RCCAL_COARSE_VALUE);
-writeExtAddrSPI(RCCAL_OFFSET, RCCAL_OFFSET_VALUE);
-writeExtAddrSPI(FREQOFF1, FREQOFF1_VALUE);
-writeExtAddrSPI(FREQOFF0, FREQOFF0_VALUE);
-writeExtAddrSPI(FREQ2, FREQ2_VALUE);
-writeExtAddrSPI(FREQ1, FREQ1_VALUE);
-writeExtAddrSPI(FREQ0, FREQ0_VALUE);
-writeExtAddrSPI(IF_ADC2, IF_ADC2_VALUE);
-writeExtAddrSPI(IF_ADC1, IF_ADC1_VALUE);
-writeExtAddrSPI(IF_ADC0, IF_ADC0_VALUE);
-writeExtAddrSPI(FS_DIG1, FS_DIG1_VALUE);
-writeExtAddrSPI(FS_DIG0, FS_DIG0_VALUE);
-writeExtAddrSPI(FS_CAL3, FS_CAL3_VALUE);
-writeExtAddrSPI(FS_CAL2, FS_CAL2_VALUE);
-writeExtAddrSPI(FS_CAL1, FS_CAL1_VALUE);
-writeExtAddrSPI(FS_CAL0, FS_CAL0_VALUE);
-writeExtAddrSPI(FS_CHP, FS_CHP_VALUE);
-writeExtAddrSPI(FS_DIVTWO, FS_DIVTWO_VALUE);
-writeExtAddrSPI(FS_DSM1, FS_DSM1_VALUE);
-writeExtAddrSPI(FS_DSM0, FS_DSM0_VALUE);
-writeExtAddrSPI(FS_DVC1, FS_DVC1_VALUE);
-writeExtAddrSPI(FS_DVC0, FS_DVC0_VALUE);
-writeExtAddrSPI(FS_LBI, FS_LBI_VALUE);
-writeExtAddrSPI(FS_PFD, FS_PFD_VALUE);
-writeExtAddrSPI(FS_PRE, FS_PRE_VALUE);
-writeExtAddrSPI(FS_REG_DIV_CML, FS_REG_DIV_CML_VALUE);
-writeExtAddrSPI(FS_SPARE, FS_SPARE_VALUE);
-writeExtAddrSPI(FS_VCO4, FS_VCO4_VALUE);
-writeExtAddrSPI(FS_VCO3, FS_VCO3_VALUE);
-writeExtAddrSPI(FS_VCO2, FS_VCO2_VALUE);
-writeExtAddrSPI(FS_VCO1, FS_VCO1_VALUE);
-writeExtAddrSPI(FS_VCO0, FS_VCO0_VALUE);
-writeExtAddrSPI(GBIAS6, GBIAS6_VALUE);
-writeExtAddrSPI(GBIAS5, GBIAS5_VALUE);
-writeExtAddrSPI(GBIAS4, GBIAS4_VALUE);
-writeExtAddrSPI(GBIAS3, GBIAS3_VALUE);
-writeExtAddrSPI(GBIAS2, GBIAS2_VALUE);
-writeExtAddrSPI(GBIAS1, GBIAS1_VALUE);
-writeExtAddrSPI(GBIAS0, GBIAS0_VALUE);
-writeExtAddrSPI(IFAMP, IFAMP_VALUE);
-writeExtAddrSPI(LNA, LNA_VALUE);
-writeExtAddrSPI(RXMIX, RXMIX_VALUE);
-writeExtAddrSPI(XOSC5, XOSC5_VALUE);
-writeExtAddrSPI(XOSC4, XOSC4_VALUE);
-writeExtAddrSPI(XOSC3, XOSC3_VALUE);
-writeExtAddrSPI(XOSC2, XOSC2_VALUE);
-writeExtAddrSPI(XOSC1, XOSC1_VALUE);
-writeExtAddrSPI(XOSC0, XOSC0_VALUE);
-writeExtAddrSPI(ANALOG_SPARE, ANALOG_SPARE_VALUE);
-writeExtAddrSPI(PA_CFG3, PA_CFG3_VALUE);
-writeExtAddrSPI(WOR_TIME1, WOR_TIME1_VALUE);
-writeExtAddrSPI(WOR_TIME0, WOR_TIME0_VALUE);
-writeExtAddrSPI(WOR_CAPTURE1, WOR_CAPTURE1_VALUE);
-writeExtAddrSPI(WOR_CAPTURE0, WOR_CAPTURE0_VALUE);
-writeExtAddrSPI(BIST, BIST_VALUE);
-writeExtAddrSPI(DCFILTOFFSET_I1, DCFILTOFFSET_I1_VALUE);
-writeExtAddrSPI(DCFILTOFFSET_I0, DCFILTOFFSET_I0_VALUE);
-writeExtAddrSPI(DCFILTOFFSET_Q1, DCFILTOFFSET_Q1_VALUE);
-writeExtAddrSPI(DCFILTOFFSET_Q0, DCFILTOFFSET_Q0_VALUE);
-writeExtAddrSPI(IQIE_I1, IQIE_I1_VALUE);
-writeExtAddrSPI(IQIE_I0, IQIE_I0_VALUE);
-writeExtAddrSPI(IQIE_Q1, IQIE_Q1_VALUE);
-writeExtAddrSPI(IQIE_Q0, IQIE_Q0_VALUE);
-writeExtAddrSPI(RSSI1, RSSI1_VALUE);
-writeExtAddrSPI(RSSI0, RSSI0_VALUE);
-writeExtAddrSPI(MARCSTATE, MARCSTATE_VALUE);
-writeExtAddrSPI(LQI_VAL, LQI_VAL_VALUE);
-writeExtAddrSPI(PQT_SYNC_ERR, PQT_SYNC_ERR_VALUE);
-writeExtAddrSPI(DEM_STATUS, DEM_STATUS_VALUE);
-writeExtAddrSPI(FREQOFF_EST1, FREQOFF_EST1_VALUE);
-writeExtAddrSPI(FREQOFF_EST0, FREQOFF_EST0_VALUE);
-writeExtAddrSPI(AGC_GAIN3, AGC_GAIN3_VALUE);
-writeExtAddrSPI(AGC_GAIN2, AGC_GAIN2_VALUE);
-writeExtAddrSPI(AGC_GAIN1, AGC_GAIN1_VALUE);
-writeExtAddrSPI(AGC_GAIN0, AGC_GAIN0_VALUE);
-writeExtAddrSPI(CFM_RX_DATA_OUT, CFM_RX_DATA_OUT_VALUE);
-writeExtAddrSPI(CFM_TX_DATA_IN, CFM_TX_DATA_IN_VALUE);
-writeExtAddrSPI(ASK_SOFT_RX_DATA, ASK_SOFT_RX_DATA_VALUE);
-writeExtAddrSPI(RNDGEN, RNDGEN_VALUE);
-writeExtAddrSPI(MAGN2, MAGN2_VALUE);
-writeExtAddrSPI(MAGN1, MAGN1_VALUE);
-writeExtAddrSPI(MAGN0, MAGN0_VALUE);
-writeExtAddrSPI(ANG1, ANG1_VALUE);
-writeExtAddrSPI(ANG0, ANG0_VALUE);
-writeExtAddrSPI(CHFILT_I2, CHFILT_I2_VALUE);
-writeExtAddrSPI(CHFILT_I1, CHFILT_I1_VALUE);
-writeExtAddrSPI(CHFILT_I0, CHFILT_I0_VALUE);
-writeExtAddrSPI(CHFILT_Q2, CHFILT_Q2_VALUE);
-writeExtAddrSPI(CHFILT_Q1, CHFILT_Q1_VALUE);
-writeExtAddrSPI(CHFILT_Q0, CHFILT_Q0_VALUE);
-writeExtAddrSPI(GPIO_STATUS, GPIO_STATUS_VALUE);
-writeExtAddrSPI(FSCAL_CTRL, FSCAL_CTRL_VALUE);
-writeExtAddrSPI(PHASE_ADJUST, PHASE_ADJUST_VALUE);
-writeExtAddrSPI(PARTNUMBER, PARTNUMBER_VALUE);
-writeExtAddrSPI(PARTVERSION, PARTVERSION_VALUE);
-writeExtAddrSPI(SERIAL_STATUS, SERIAL_STATUS_VALUE);
-writeExtAddrSPI(MODEM_STATUS1, MODEM_STATUS1_VALUE);
-writeExtAddrSPI(MODEM_STATUS0, MODEM_STATUS0_VALUE);
-writeExtAddrSPI(MARC_STATUS1, MARC_STATUS1_VALUE);
-writeExtAddrSPI(MARC_STATUS0, MARC_STATUS0_VALUE);
-writeExtAddrSPI(PA_IFAMP_TEST, PA_IFAMP_TEST_VALUE);
-writeExtAddrSPI(FSRF_TEST, FSRF_TEST_VALUE);
-writeExtAddrSPI(PRE_TEST, PRE_TEST_VALUE);
-writeExtAddrSPI(PRE_OVR, PRE_OVR_VALUE);
-writeExtAddrSPI(ADC_TEST, ADC_TEST_VALUE);
-writeExtAddrSPI(DVC_TEST, DVC_TEST_VALUE);
-writeExtAddrSPI(ATEST, ATEST_VALUE);
-writeExtAddrSPI(ATEST_LVDS, ATEST_LVDS_VALUE);
-writeExtAddrSPI(ATEST_MODE, ATEST_MODE_VALUE);
-writeExtAddrSPI(XOSC_TEST1, XOSC_TEST1_VALUE);
-writeExtAddrSPI(XOSC_TEST0, XOSC_TEST0_VALUE);
-writeExtAddrSPI(RXFIRST, RXFIRST_VALUE);
-writeExtAddrSPI(TXFIRST, TXFIRST_VALUE);
-writeExtAddrSPI(RXLAST, RXLAST_VALUE);
-writeExtAddrSPI(TXLAST, TXLAST_VALUE);
-writeExtAddrSPI(NUM_TXBYTES, NUM_TXBYTES_VALUE);
-writeExtAddrSPI(NUM_RXBYTES, NUM_RXBYTES_VALUE);
-writeExtAddrSPI(FIFO_NUM_TXBYTES, FIFO_NUM_TXBYTES_VALUE);
-writeExtAddrSPI(FIFO_NUM_RXBYTES, FIFO_NUM_RXBYTES_VALUE);
+writeExtAddrSPI(FREQOFF_CFG, FREQOFF_CFG_VALUE);   // Frequency Offset Correction Configuration
+writeExtAddrSPI(FREQ2, FREQ2_VALUE);         // Frequency Configuration [23:16]
+writeExtAddrSPI(FREQ1, FREQ1_VALUE);         // Frequency Configuration [15:8]
+writeExtAddrSPI(FREQ0, FREQ0_VALUE);         // Frequency Configuration [7:0]
+writeExtAddrSPI(FS_DIG1, FS_DIG1_VALUE);       // Frequency Synthesizer Digital Reg. 1
+writeExtAddrSPI(FS_DIG0, FS_DIG0_VALUE);       // Frequency Synthesizer Digital Reg. 0
+writeExtAddrSPI(FS_CAL1, FS_CAL1_VALUE);       // Frequency Synthesizer Calibration Reg. 1
+writeExtAddrSPI(FS_CAL0, FS_CAL0_VALUE);       // Frequency Synthesizer Calibration Reg. 0
+writeExtAddrSPI(FS_DIVTWO, FS_DIVTWO_VALUE);     // Frequency Synthesizer Divide by 2
+writeExtAddrSPI(FS_DSM0, FS_DSM0_VALUE);       // FS Digital Synthesizer Module Configuration Reg. 0
+writeExtAddrSPI(FS_DVC0, FS_DVC0_VALUE);       // Frequency Synthesizer Divider Chain Configuration ..
+writeExtAddrSPI(FS_PFD, FS_PFD_VALUE);        // Frequency Synthesizer Phase Frequency Detector Con..
+writeExtAddrSPI(FS_PRE, FS_PRE_VALUE);        // Frequency Synthesizer Prescaler Configuration
+writeExtAddrSPI(FS_REG_DIV_CML, FS_REG_DIV_CML_VALUE);// Frequency Synthesizer Divider Regulator Configurat..
+writeExtAddrSPI(FS_SPARE, FS_SPARE_VALUE);      // Frequency Synthesizer Spare
+writeExtAddrSPI(FS_VCO0, FS_VCO0_VALUE);       // FS Voltage Controlled Oscillator Configuration Reg..
+writeExtAddrSPI(XOSC5, XOSC5_VALUE);         // Crystal Oscillator Configuration Reg. 5
+writeExtAddrSPI(XOSC1, XOSC1_VALUE);         // Crystal Oscillator Configuration Reg. 1
+writeExtAddrSPI(PARTNUMBER, PARTNUMBER_VALUE);    // Part Number
+writeExtAddrSPI(PARTVERSION, PARTVERSION_VALUE);   // Part Revision
+writeExtAddrSPI(MODEM_STATUS1, MODEM_STATUS1_VALUE); // Modem Status Reg. 1
 
 
 
@@ -473,8 +317,6 @@ writeExtAddrSPI(FIFO_NUM_RXBYTES, FIFO_NUM_RXBYTES_VALUE);
  
 void FIFOFlush(){
   strobeSPI(SFRX); // Flush the RX FIFO
-  delay(100);
   strobeSPI(SFTX); // Flush the TX FIFO
-  delay(100);
 }
  
