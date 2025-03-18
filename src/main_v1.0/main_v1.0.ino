f365092ade30fdfbd1f2e7f0c18dc00a3f23fbd9
koketsu <kt.class.jar@gmail.com>
2025-03-19 04:04:19 +0900

    EM sucseed

--- src/main_v1.0/main_v1.0.ino
+++ src/main_v1.0/main_v1.0.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
 #include "CC1120.h"
 #include "IoExpander.h"
-// #include "CommunicationDevice.h"
+#include "CommunicationDevice.h"
 
 CC1120Class CC1120;
 IoExpander  IE;
-// CommunicationDevice CD;
+CommunicationDevice CD;
 
 bool ret = 1;
 
@@ -19,23 +19,23 @@ void setup() {
   Serial.print("after begin()");
   Serial.println(ret);
 
-  // CD.switchMode(RECEIVE, FSK);
+  CD.switchMode(SEND, FSK);
 }
 
 void loop() {
-  Serial.println(CC1120.marcstate());
+  // Serial.println(CC1120.marcstate());
   // put your main code here, to run repeatedly:
-  // Serial.println("loop");
-  // uint8_t payload[1005]= "In twilight's glow, the silence calls, a whisper drifting, soft and small. The wind, it dances, wild and free, a song of time, a melody. The stars appear, their light so bright, a quiet shimmer in the night. And in the breeze, a secret shared, a fleeting thought, a dream declared. The world around us slows its pace, as shadows hide from dawn's embrace. Yet in the stillness, hearts will find the endless rhythm, intertwined. Beneath the sky, so vast and wide, we walk this earth, side by side. Unspoken words, yet understood, the quiet bond of soul and blood. Through all the years, both calm and storm, we seek the truth, we search for warm embraces held in fleeting moments, lost within the world's vast currents. But still, the whispers call again, to guide us through the sorrow, pain. A journey long, yet full of grace, as time and tide, they interlace. So listen close, and feel the wind, it carries dreams, where hope begins. For in the quiet of the night, we find our peace, and take our flight.";
-  // ret = CC1120.TX(payload, 1005);
-  // Serial.println(ret);
-  // delay(5000);
-
-  uint8_t data[128];
-  ret = CC1120.recvUL(data);
+  Serial.println("loop");
+  uint8_t payload[1005]= "In twilight's glow, the silence calls, a whisper drifting, soft and small. The wind, it dances, wild and free, a song of time, a melody. The stars appear, their light so bright, a quiet shimmer in the night. And in the breeze, a secret shared, a fleeting thought, a dream declared. The world around us slows its pace, as shadows hide from dawn's embrace. Yet in the stillness, hearts will find the endless rhythm, intertwined. Beneath the sky, so vast and wide, we walk this earth, side by side. Unspoken words, yet understood, the quiet bond of soul and blood. Through all the years, both calm and storm, we seek the truth, we search for warm embraces held in fleeting moments, lost within the world's vast currents. But still, the whispers call again, to guide us through the sorrow, pain. A journey long, yet full of grace, as time and tide, they interlace. So listen close, and feel the wind, it carries dreams, where hope begins. For in the quiet of the night, we find our peace, and take our flight.";
+  ret = CC1120.TX(payload, 1005);
   Serial.println(ret);
-  if(ret == 1){
-    for(int i=0; i<19; i++) Serial.println((char)data[i]);
-  }
-  delay(3000);
+  delay(5000);
+
+  // uint8_t data[128];
+  // ret = CC1120.recvUL(data);
+  // Serial.println(ret);
+  // if(ret == 1){
+  //   for(int i=0; i<19; i++) Serial.println((char)data[i]);
+  // }
+  // delay(3000);
 }
